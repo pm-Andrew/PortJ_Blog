bf71dad673c9ea4ed8bab27afed7fa0c68f2a705
Andrew <a.sabourin@students.clark.edu>
2026-06-29 12:40:55 -0700

    add comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // add your project configuration here ie site, integrations, build options, etc.
+    // site: 'https://example.com',
 });
