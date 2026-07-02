dc568a9759deb0d807e5cb06b525c63844731735
Andrew <a.sabourin@students.clark.edu>
2026-07-02 13:44:14 -0700

    refactor posts to collections

--- src/blog/post-1.md
+++ src/blog/post-1.md
@@ -1,6 +1,5 @@
 ---
 # Formatter -- data—including tags and a post image
-layout: ../../layouts/MarkdownPostLayout.astro
 title: 'My First Blog Post'
 pubDate: 2022-07-01
 description: 'This is the first post of my new Astro blog.'
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,18 @@
 // This is an example of an RSS feed for your blog. You can customize this to fit your needs.
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const posts = await getCollection("blog");
     return rss({
         title: 'Astro Learner | Example Blog',
         description: 'My journey learning Astro ',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.id}/`,
+        })),
         customData: `<language>en-us</language>`,
     });
 }
