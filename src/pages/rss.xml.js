c0954fa7a3be419ef548f458ce70941d8150b0ff
Andrew <a.sabourin@students.clark.edu>
2026-07-30 22:24:19 -0700

    now?

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-// This is an example of an RSS feed for your blog. You can customize this to fit your needs.
-import { getCollection } from 'astro:content';
+// // This is an example of an RSS feed for your blog. You can customize this to fit your needs.
+// import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-    const posts = await getCollection("blog");
-    return rss({
-        title: 'Astro Learner | Example Blog',
-        description: 'My journey learning Astro ',
-        site: context.site,
-        items: posts.map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.pubDate,
-            description: post.data.description,
-            link: `/posts/${post.id}/`,
-        })),
-        customData: `<language>en-us</language>`,
-    });
-}
+// export async function GET(context) {
+//     const posts = await getCollection("blog");
+//     return rss({
+//         title: 'Astro Learner | Example Blog',
+//         description: 'My journey learning Astro ',
+//         site: context.site,
+//         items: posts.map((post) => ({
+//             title: post.data.title,
+//             pubDate: post.data.pubDate,
+//             description: post.data.description,
+//             link: `/posts/${post.id}/`,
+//         })),
+//         customData: `<language>en-us</language>`,
+//     });
+// }
